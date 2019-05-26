5ac26798575eb676f645b66b4eb66498cc015b1e
HoneyJung <kinkin21c@u.sogang.ac.kr>
2019-05-26 08:26:03 +0000

    commit DB

---
+++ server/data.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8  -*-
+from konlpy.corpus import kolaw
+from konlpy.corpus import kobill
+#import MySQLdb
+import pymysql
+import re
+
+
+
+conn = pymysql.connect(host = '18.222.102.66', user = 'root', password = '1234' ,db = 'CAP')
+# host = DB주소(localhost 또는 ip주소), user = DB id, password = DB password, db = DB명
+curs = conn.cursor()
+
+
+c = kolaw.open('constitution.txt').read()
+#print(type(c))
+c = re.sub(r'\n',' ',c)
+c = re.sub('  ',' ',c)
+k = c.split(' ')
+n = []
+for i in k:
+    if len(i) == 3 and i.isalpha() == True :
+        n.append(i)
+
+for j in n:
+    sql = "insert into CAP.data(word,flag) VALUES('"+j+"',1)"
+    curs.execute(sql)
+
+
+sql = "SELECT * FROM data"
+curs.execute(sql) # 쿼리문 실행
+
+rows = curs.fetchall() # 데이터 패치
+
+for i in rows :
+     print(i)
+
+conn.close()
+
+
+'''
+c = kolaw.open('1809890.txt').read()
+c = re.sub(r'\n',' ',c)
+c = re.sub('  ',' ',c)
+k = c.split(' ')
+for i in k:
+    if len(i) == 3:
+        n.append(i)
+'''
+
+
+#print (n)
