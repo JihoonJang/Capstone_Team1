a590129b911ba6aeb7b7800c30f6f987c22dce31
HoneyJung <kinkin21c@u.sogang.ac.kr>
2019-06-03 06:46:51 +0000

    img2

--- server/web.js
+++ server/web.js
@@ -25,7 +25,8 @@ let options = {
 };
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
 app.get('/',function(req,res){
 	console.log("body : ", req.body);
