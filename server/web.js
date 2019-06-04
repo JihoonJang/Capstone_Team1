ba7367fffe4f1c8ab58d72e535942b7e76c4254b
HoneyJung <kinkin21c@u.sogang.ac.kr>
2019-06-04 14:58:36 +0000

    i3

--- server/web.js
+++ server/web.js
@@ -6,39 +6,42 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var http = require('http');
 var requestIp = require('request-ip');
-//.const ps = require('python-shell');
-//json_data = JSON.stringify(query_data);
-/*var options = {
-	mode : 'text',
-	pythonOptions: ['-u'],
-	scriptPath: '',
-	args : ['1']
-};*/
+var fs = require('fs');
+
+const router = express.Router();
+const multer = require('multer');
+let upload = multer({dest : 'uploads/'});
+
+app.use(bodyParser.json({limit: '50mb', extended: false}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended:false}))
 
 
+router.post('/upload',upload.single('image'),(req,res,next,error) => {
+	console.log(error);
+
+});
+module.exports = router;
 let options = {
   mode: 'text',
   //thonPath: 'path/to/python',
   pythonOptions: ['-u'], // get print results in real-time
   //riptPath: 'path/to/my/scripts',
   args: ['value1', 'value2', 'value3']
-};
-app.use(bodyParser.json({limit: '50mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
-
-app.get('/',function(req,res){
+};app.get('/',function(req,res){
 	console.log("body : ", req.body);
 	res.send('Hello World!');
 })
 
 app.post('/',function(req,res){
-	console.log("body : " , req.body);
+	//var dataa = req.body;
+	var dat = JSON.stringify(req.body.data);
+	console.log("aefeafew fae e");
+	console.log("body : " , dat);
 	console.log("ip : ", req.ip);
-	res.send("양간마 양간마 안시이현 안시현 러시안룰룰룰룰룰렛 장지훈 모태솔로로로로로로 양간마 안시현 인간티머니");
-	var fs = require('fs');
-	fs.writeFile('file.txt',req.data,'utf8', function(error,data){
+	fs.writeFile('file.txt',dat,'utf8', function(error,data){
 		console.log(error)
 	});
+	res.send("양간마 양간마 안시이현 안시현 러시안룰룰룰룰룰렛 장지훈 모태솔로로로로로로 양간마 안시현 인간티머니");
 	let options = {
   			mode: 'text',
   			//thonPath: 'path/to/python',
